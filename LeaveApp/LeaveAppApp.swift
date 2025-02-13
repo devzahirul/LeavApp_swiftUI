355f141cfe23794b2895ba1012da932533cb638f
zahirulAIIUB <jislam150@gmail.com>
2025-02-13 17:35:35 +0600

    Initial Commit

---
+++ LeaveApp/LeaveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaveAppApp.swift
+//  LeaveApp
+//
+//  Created by lynkto_1 on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LeaveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
